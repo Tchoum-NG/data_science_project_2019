321f6e96e08013bc0dce0eed48a2ecf2b2aad368
franzmotzkus <franzmotzkus@campus.tu-berlin.de>
2019-10-19 11:57:52 +0300

    draft for popover

--- website/index.js
+++ website/index.js
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
       datap.push(row);
     })
     .on('end', () => {
-      //console.log(JSON.stringify(datap[0]))
+      console.log(JSON.stringify(datap[0]))
       console.log('successfully read the csv file');
       res.render('index', {
         datap1: JSON.stringify(datap),
